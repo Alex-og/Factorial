7727cfd57e12452b036a1743ee30ad15678de2a0
Oleksandr Haleta <alexhaleta99@gmail.com>
2021-08-27 17:02:38 +0300

    added tests to Match

--- src/main/java/findMatch/Match.java
+++ src/main/java/findMatch/Match.java
@@ -5,10 +5,6 @@ import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class Match<T> {
-    public static void main(String[] args) {
-        List<String> inputList = null;
-        System.out.println(new Match<String>().findMatch(inputList, 2, (x -> x.contains("b"))));
-    }
 
     public List<T> findMatch(List<T> list, int n, Predicate<T> predicate) {
         return list == null ? List.of() : list.stream().filter(predicate).limit(n).collect(Collectors.toList());
